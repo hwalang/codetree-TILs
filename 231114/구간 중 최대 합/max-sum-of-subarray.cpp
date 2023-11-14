f529fa7d02242350220eea1ce5e005e963b76b7f
hwalang <51149110+hwalang@users.noreply.github.com>
2023-11-14 09:02:53 +0900

    Title: 구간 중 최대 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231114/구간 중 최대 합/max-sum-of-subarray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX_N 100
+
+int N, K, ans;
+int nums[MAX_N];
+
+int main() {
+
+	cin >> N >> K;
+	for (int i = 0; i < N; ++i)
+		cin >> nums[i];
+
+	for (int i = 0; i < N - K; ++i) {
+		int sum = 0;
+		for (int j = i; j < i + K; ++j) {
+			sum += nums[j];
+		}
+
+		ans = max(ans, sum);
+	}
+
+	cout << ans;
+
+	return 0;
+}
