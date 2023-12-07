0dd04324c1452f8fd66f7dcedd01d05e8703339c
hwalang <51149110+hwalang@users.noreply.github.com>
2023-12-07 17:15:47 +0900

    Title: G or H 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231207/G or H 2/G-or-H-2.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+#define MAX_LOC 100
+
+int N;
+char loc[MAX_LOC + 1];
+
+int main() {
+
+	cin >> N;
+	for (int i = 0; i < N; ++i) {
+		int idx; char placard;
+		cin >> idx >> placard;
+
+		loc[idx] = placard;
+	}
+
+	int max_picture_size = 0;
+	// 1. G로만 구성 + 사진의 최대 크기
+	for (int i = 0; i < MAX_LOC; ++i) {
+		if (loc[i] != 'G') continue;
+		int start_loc = i;
+		int end_loc = i;
+		for (int j = i + 1; j <= MAX_LOC; ++j) {
+			if (loc[j] == 'G') {
+				end_loc = j;
+			}
+			else if (loc[j] == 'H') break;
+		}
+		int picture_size = end_loc - start_loc;
+		max_picture_size = max(max_picture_size, picture_size);
+	}
+
+	// 2. H로만 구성 + 사진의 최대 크기
+	for (int i = 0; i < MAX_LOC; ++i) {
+		if (loc[i] != 'H') continue;
+		int start_loc = i;
+		int end_loc = i;
+		for (int j = i + 1; j <= MAX_LOC; ++j) {
+			if (loc[j] == 'H') {
+				end_loc = j;
+			}
+			else if (loc[j] == 'G') break;
+		}
+		int picture_size = end_loc - start_loc;
+		max_picture_size = max(max_picture_size, picture_size);
+	}
+
+	// 3. G, H가 같은 수로 구성 + 사진의 최대 크기
+	for (int i = 0; i < MAX_LOC; ++i) {
+		int cntG = 0;
+		int cntH = 0;
+		if (loc[i] == 'G') ++cntG;
+		else if (loc[i] == 'H') ++cntH;
+		else continue;
+		int start_loc = i;
+		int end_loc = i;
+
+		for (int j = i + 1; j <= MAX_LOC; ++j) {
+			if (loc[j] == 'G') ++cntG;
+			else if (loc[j] == 'H') ++cntH;
+			else continue;
+
+			if (abs(cntG - cntH) >= 2) break;
+			else if ( (loc[j] == 'G' || loc[j] == 'H') && cntG == cntH) end_loc = j;
+		}
+
+		int picture_size = end_loc - start_loc;
+		max_picture_size = max(max_picture_size, picture_size);
+	}
+
+	cout << max_picture_size;
+
+	return 0;
+}
