c6a4c5a93f06a687eaea7bc20db90c6988a19835
hwalang <51149110+hwalang@users.noreply.github.com>
2024-04-25 22:20:50 +0900

    Title: 정수 명령 처리, Time: 17ms, Memory: 0MB, Status: Passed - CodeTree

--- 240425/정수 명령 처리/process-numeric-commands.cpp
+++ 240425/정수 명령 처리/process-numeric-commands.cpp
@@ -6,38 +6,40 @@ using namespace std;
 
 int main() {
     stack<int> _stack;
+
     int size;
-    string order;
-    int input;
-
     cin >> size;
-    while (size <= 0)
-    {
-        cin >> order;
-        switch (order)
-        {
-            case "push":
-                cin >> input;
-                _stack.push(input);
-                break;
-            case "size":
-                cout << _stack.size() << endl;
-                break;
-            case "empty":
-                int out = _stack.empty() ? 1 : 0;
-                cout << out << endl;
-                break;
-            case "pop":
-                int out = _stack.top();
-                _stack.pop();
-                cout << out << endl;
-                break;
-            case "top":
-                cout << _stack.top() << endl;
-                break;
-        }
-        
 
+    while (size > 0)
+    {
+        int out;
+        string order;
+        int input;
+        cin >> order;
+        if (order == "push")
+        {
+            cin >> input;
+            _stack.push(input);
+        }
+        else if (order == "size")
+        {
+            cout << _stack.size() << endl;
+        }
+        else if (order == "empty")
+        {
+            out = _stack.empty() ? 1 : 0;   
+            cout << out << endl;
+        }
+        else if (order == "pop")
+        {
+            out = _stack.top();
+            _stack.pop();
+            cout << out << endl;
+        }
+        else
+        {
+            cout << _stack.top() << endl;
+        }
         --size;
     }
 
