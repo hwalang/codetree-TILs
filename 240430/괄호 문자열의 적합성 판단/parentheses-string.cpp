772a8328eb73030a85f6b47add552537d8acb70e
hwalang <51149110+hwalang@users.noreply.github.com>
2024-04-30 03:43:14 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240430/괄호 문자열의 적합성 판단/parentheses-string.cpp
+++ 240430/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -11,12 +11,17 @@ bool solution()
 {
     for (int i = 0; i < input.size(); ++i)
     {
-        if (input[i] == '(' && _stack.empty())
+        if (input[i] == '(')
         {
-            _stack.push(input[i]);
+            if (_stack.empty())
+                _stack.push(input[i]);
+            else
+                return false;
         }
-        else if (input[i] == ')' && !_stack.empty())
+        else if (input[i] == ')')
         {
+            if (_stack.empty()) return false;
+            
             if (_stack.top() == '(')
             {
                 _stack.pop();
